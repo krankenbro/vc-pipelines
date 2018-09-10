47f15462436f004af8d7445854043c470086df22
Alexander Morogov <asmorogov@gmail.com>
2018-09-10 15:21:15 +0200

    -organization

--- src/jobs/scripts/Packaging.groovy
+++ src/jobs/scripts/Packaging.groovy
@@ -218,7 +218,7 @@ class Packaging {
         def coverageFolder = Utilities.getCoverageFolder(context)
         context.withSonarQubeEnv('VC Sonar Server') {
             // Due to SONARMSBRU-307 value of sonar.host.url and credentials should be passed on command line
-            context.bat "\"${sqScannerMsBuildHome}\\SonarQube.Scanner.MSBuild.exe\" begin /d:\"sonar.branch=${context.env.BRANCH_NAME}\" /n:\"${fullJobName}\" /k:\"${fullJobName}\" /d:\"sonar.organization=virtocommerce\" /d:sonar.host.url=%SONAR_HOST_URL% /d:sonar.login=%SONAR_AUTH_TOKEN% /d:sonar.cs.vscoveragexml.reportsPaths=\"${coverageFolder}\\VisualStudio.Unit.coveragexml\""
+            context.bat "\"${sqScannerMsBuildHome}\\SonarQube.Scanner.MSBuild.exe\" begin /d:\"sonar.branch=${context.env.BRANCH_NAME}\" /n:\"${fullJobName}\" /k:\"${fullJobName}\" /d:sonar.host.url=%SONAR_HOST_URL% /d:sonar.login=%SONAR_AUTH_TOKEN% /d:sonar.cs.vscoveragexml.reportsPaths=\"${coverageFolder}\\VisualStudio.Unit.coveragexml\"" // -/d:\"sonar.organization=virtocommerce\"
         }        
     }
 
