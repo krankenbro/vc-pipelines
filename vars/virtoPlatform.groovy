d9a0522d9eaa02dc906167b27cafd60eb9f929d3
Alexander Morogov <asmorogov@gmail.com>
2018-09-10 10:06:31 +0200

    sonarqube

--- vars/virtoPlatform.groovy
+++ vars/virtoPlatform.groovy
@@ -31,9 +31,9 @@ def call(body){
 			}
 			else  {
 				bat "nuget restore ${solution}"
-				bat 'SonarQube.Scanner.MSBuild.exe begin /k:"vc-platform" /d:sonar.host.url="http://localhost:9000" /d:sonar.login="platform"'
+				bat 'SonarQube.Scanner.MSBuild.exe begin /k:"vc-platform" /d:sonar.host.url="http://localhost:9000" /d:sonar.login="e6514eb74da2151591cde1a0205584ac47f12190"'
 				bat "\"${tool 'DefaultMSBuild'}\\msbuild.exe\" \"${solution}\" /p:Configuration=Debug /p:Platform=\"Any CPU\" /t:rebuild /m"
-				bat 'SonarQube.Scanner.MSBuild.exe end /d:sonar.login="platform"'
+				bat 'SonarQube.Scanner.MSBuild.exe end /d:sonar.login="e6514eb74da2151591cde1a0205584ac47f12190"'
 			}
 		}
 		def tests = Utilities.getTestDlls(this)
