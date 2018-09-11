8dfba6efd824174af93de4c6da1b70e6adc1abca
Alexander Morogov <asmorogov@gmail.com>
2018-09-11 11:49:31 +0200

    SonarScanner.MSBuild.exe

--- src/jobs/scripts/Packaging.groovy
+++ src/jobs/scripts/Packaging.groovy
@@ -218,7 +218,7 @@ class Packaging {
         def coverageFolder = Utilities.getCoverageFolder(context)
         context.withSonarQubeEnv('VC Sonar Server') {
             // Due to SONARMSBRU-307 value of sonar.host.url and credentials should be passed on command line
-            context.bat "\"${sqScannerMsBuildHome}\\SonarQube.Scanner.MSBuild.exe\" begin /d:\"sonar.branch=${context.env.BRANCH_NAME}\" /n:\"${fullJobName}\" /k:\"${fullJobName}\" /d:sonar.host.url=%SONAR_HOST_URL% /d:sonar.login=%SONAR_AUTH_TOKEN% /d:sonar.cs.vscoveragexml.reportsPaths=\"${coverageFolder}\\VisualStudio.Unit.coveragexml\"" // -/d:\"sonar.organization=virtocommerce\"
+            context.bat "\"${sqScannerMsBuildHome}\\SonarScanner.MSBuild.exe\" begin /d:\"sonar.branch=${context.env.BRANCH_NAME}\" /n:\"${fullJobName}\" /k:\"${fullJobName}\" /d:sonar.host.url=%SONAR_HOST_URL% /d:sonar.login=%SONAR_AUTH_TOKEN% /d:sonar.cs.vscoveragexml.reportsPaths=\"${coverageFolder}\\VisualStudio.Unit.coveragexml\"" // -/d:\"sonar.organization=virtocommerce\"
         }        
     }
 
@@ -227,7 +227,7 @@ class Packaging {
         def sqScannerMsBuildHome = context.tool 'Scanner for MSBuild'
         def fullJobName = Utilities.getRepoName(context)
         context.withSonarQubeEnv('VC Sonar Server') {
-            context.bat "\"${sqScannerMsBuildHome}\\SonarQube.Scanner.MSBuild.exe\" end /d:sonar.login=%SONAR_AUTH_TOKEN%" //https://jira.sonarsource.com/browse/SONARMSBRU-378
+            context.bat "\"${sqScannerMsBuildHome}\\SonarScanner.MSBuild.exe\" end /d:sonar.login=%SONAR_AUTH_TOKEN%" //https://jira.sonarsource.com/browse/SONARMSBRU-378
         }        
     }
 
