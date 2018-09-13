244405f8c97aa4189ae1921b6d8f782b9a872d80
Alexander Morogov <asmorogov@gmail.com>
2018-09-13 17:31:38 +0200

    email test

--- vars/virtoPlatform.groovy
+++ vars/virtoPlatform.groovy
@@ -29,61 +29,62 @@ def call(body){
 					checkout scm;
 				}
 			}
+		
+			stage ('Build & Analyze') {
+				timestamps {
+					if(projectType == "NETCORE2") { //storefront
+						Packaging.startAnalyzer(this)
+						bat "\"${tool 'DefaultMSBuild'}\\msbuild.exe\" \"${solution}\" /p:Configuration=Debug /p:Platform=\"Any CPU\" /t:restore /t:rebuild /m"
+					}
+					else  { //platform
+						bat "nuget restore ${solution}"
+						Packaging.startAnalyzer(this)
+						bat "\"${tool 'DefaultMSBuild'}\\msbuild.exe\" \"${solution}\" /p:Configuration=Debug /p:Platform=\"Any CPU\" /t:rebuild /m"
+					}
+				}
+			}
+			def tests = Utilities.getTestDlls(this)
+			if(projectType == "NETCORE2" && tests.size() < 1) {
+				tests = findFiles(glob: '**\\bin\\Debug\\*\\*Tests.dll')
+			}
+			if(tests.size() > 0)
+			{
+				stage('Tests') {
+					timestamps { 
+						String paths = ""
+						String traits = "-trait \"category=ci\" -trait \"category=Unit\""
+						String resultsFileName = "xUnit.UnitTests.xml"
+						for(int i = 0; i < tests.size(); i++)
+						{
+							def test = tests[i]
+							paths += "\"$test.path\" "
+						}
+						if(projectType == "NETCORE2") {
+							bat "dotnet vstest ${paths} --TestCaseFilter:\"Category=Unit\""
+						}
+						else {
+							bat "\"${env.XUnit}\\xunit.console.exe\" ${paths} -xml \"${resultsFileName}\" ${traits} -parallel none"
+						}
+					}
+				}
+			}
+			stage('Stop Analyze') {
+				timestamps {
+					Packaging.endAnalyzer(this)
+				}
+			}
+
+			// No need to occupy a node
+			stage("Quality Gate"){
+				timestamps {
+					Packaging.checkAnalyzerGate(this)
+				}
+			}
 		}
 		catch(err) {
-			mail body: "Calling nonexistent method\n${err.getMessage()}", from: 'sasha@morogov.ru', subject: 'error', to: 'asmorogov@gmail.com'
+			mail body: "Calling nonexistent method\n${err.getMessage()}", from: 'sasha@morogov.ru', subject: "${env.STAGE_NAME} failed", to: 'asmorogov@gmail.com'
 			currentBuild.result = 'ABORTED'
-			error('Stopping early…')
-		}
-		stage ('Build & Analyze') {
-			timestamps {
-				if(projectType == "NETCORE2") { //storefront
-					Packaging.startAnalyzer(this)
-					bat "\"${tool 'DefaultMSBuild'}\\msbuild.exe\" \"${solution}\" /p:Configuration=Debug /p:Platform=\"Any CPU\" /t:restore /t:rebuild /m"
-				}
-				else  { //platform
-					bat "nuget restore ${solution}"
-					Packaging.startAnalyzer(this)
-					bat "\"${tool 'DefaultMSBuild'}\\msbuild.exe\" \"${solution}\" /p:Configuration=Debug /p:Platform=\"Any CPU\" /t:rebuild /m"
-				}
-			}
-		}
-		def tests = Utilities.getTestDlls(this)
-		if(projectType == "NETCORE2" && tests.size() < 1) {
-			tests = findFiles(glob: '**\\bin\\Debug\\*\\*Tests.dll')
-		}
-		if(tests.size() > 0)
-		{
-			stage('Tests') {
-				timestamps { 
-					String paths = ""
-					String traits = "-trait \"category=ci\" -trait \"category=Unit\""
-					String resultsFileName = "xUnit.UnitTests.xml"
-					for(int i = 0; i < tests.size(); i++)
-					{
-						def test = tests[i]
-						paths += "\"$test.path\" "
-					}
-					if(projectType == "NETCORE2") {
-						bat "dotnet vstest ${paths} --TestCaseFilter:\"Category=Unit\""
-					}
-					else {
-						bat "\"${env.XUnit}\\xunit.console.exe\" ${paths} -xml \"${resultsFileName}\" ${traits} -parallel none"
-					}
-				}
-			}
-		}
-		stage('Stop Analyze') {
-			timestamps {
-				Packaging.endAnalyzer(this)
-			}
-		}
-
-		// No need to occupy a node
-		stage("Quality Gate"){
-			timestamps {
-				Packaging.checkAnalyzerGate(this)
-			}
+			throw err
 		}
 	}
 }
