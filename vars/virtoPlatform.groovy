a5f2becd6a5b0fb83d502465272dd67dbd955e71
Alexander Morogov <asmorogov@gmail.com>
2018-09-18 12:22:33 +0200

    send mail fallback

--- vars/virtoPlatform.groovy
+++ vars/virtoPlatform.groovy
@@ -81,7 +81,7 @@ def call(body){
 			}
 		}
 		catch(Throwable e) {
-			Utilities.sendMail this, "FAILED", "${e.getMessage()}\n${e.getCause()}\n\n\n${currentBuild.rawBuild.getLog(Utilities.getFailedStageLog(currentBuild.rawBuild.getLog())).join("\n")}"
+			Utilities.sendMail this, "FAILED", "${e.getMessage()}\n${e.getCause()}\n\n\n${currentBuild.rawBuild.getLog(15).join("\n")}"
 			currentBuild.result = 'FAILED'
 			throw e
 		}
