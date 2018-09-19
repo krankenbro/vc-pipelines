73fadf52ef73b2efe577bfaae23cc97a6f64705c
Alexander Morogov <asmorogov@gmail.com>
2018-09-19 16:33:21 +0200

    result-> currentResult

--- vars/virtoPlatform.groovy
+++ vars/virtoPlatform.groovy
@@ -89,7 +89,7 @@ def call(body){
 		}
 		finally {
 			if(currentBuild.result != 'FAILED') {
-				Utilities.sendMail(this, "${currentBuild.result}")
+				Utilities.sendMail(this, "${currentBuild.currentResult}")
 			}
 		}
 	}
