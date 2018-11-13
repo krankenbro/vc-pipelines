fb094c850e57e64404df0f50ccb272c3d0aa5588
Alexander Morogov <asmorogov@gmail.com>
2018-11-13 15:47:29 +0200

    changes from main ci

--- resources/azure/vc-setup-modules.ps1
+++ resources/azure/vc-setup-modules.ps1
@@ -1,9 +1,9 @@
 Param(
     [parameter(Mandatory = $true)]
     $apiurl,
-    $sampleDataSrc,
     $hmacAppId,
-    $hmacSecret
+    $hmacSecret,
+    $needRestart
 )
 
 . $PSScriptRoot\utilities.ps1
@@ -15,41 +15,67 @@ if ([string]::IsNullOrWhiteSpace($hmacAppId)) {
 if ([string]::IsNullOrWhiteSpace($hmacSecret)) {
     $hmacSecret = "${env:HMAC_SECRET}"
 }
-if ([string]::IsNullOrWhiteSpace($sampleDataSrc)) {
-    $sampleDataSrc = "${env:SAMPLE_DATA}"
+
+# Initialize paths used by the script
+$modulesStateUrl = "$apiurl/api/platform/pushnotifications"
+$modulesInstallUrl = "$apiurl/api/platform/modules/autoinstall"
+$restartUrl = "$apiurl/api/platform/modules/restart"
+
+# Call homepage, to make sure site is compiled
+$initResult = Invoke-WebRequest $apiurl -UseBasicParsing
+if ($initResult.StatusCode -ne 200) {
+    # throw exception when site can't be opened
+    throw "Can't open admin site homepage"
 }
 
-$sdStateUrl = "$apiurl/api/platform/pushnotifications"
-if([string]::IsNullOrWhiteSpace($sampleDataSrc)){
-    $sdInstallUrl = "$apiurl/api/platform/sampledata/autoinstall"
-} else {
-    $sdInstallUrl = "$apiurl/api/platform/sampledata/import?url=$sampleDataSrc"
-}
-
-
+# Initiate modules installation
 $headerValue = Create-Authorization $hmacAppId $hmacSecret
 $headers = @{}
 $headers.Add("Authorization", $headerValue)
-$installResult = Invoke-RestMethod -Uri $sdInstallUrl -ContentType "application/json" -Method Post -Headers $headers
-Write-Output $installResult
+$moduleImportResult = Invoke-RestMethod $modulesInstallUrl -Method Post -Headers $headers -ErrorAction Stop
 
-$notificationId = $installResult.id
+Start-Sleep -s 1
+
+# save notification id, so we can get status of the operation
+$notificationId = $moduleImportResult.id
+
+# create status request json, we only need to get 1 and 1st notification
 $NotificationStateJson = @"
      {"Ids":["$notificationId"],"start":0, "count": 1}
 "@
 
-$notify = @{}
-do {
-    Start-Sleep -s 3
-    $state = Invoke-RestMethod "$sdStateUrl" -Body $NotificationStateJson -Method Post -ContentType "application/json" -Headers $headers
-    Write-Output $state
-    if ($state.notifyEvents -ne $null ) {
-        $notify = $state.notifyEvents
-        if ($notify.errorCount -gt 0) {
-            Write-Output $notify
-            exit 1
+$cycleCount = 0
+$startIndex = 0
+$abort = $false
+try {
+    do {
+        # Retrieve notification state
+        $moduleState = Invoke-RestMethod "$modulesStateUrl" -Body $NotificationStateJson -Method Post -ContentType "application/json" -Headers $headers
+
+        # display all statuses
+        if ($moduleState.NotifyEvents -ne $null -and $moduleState.NotifyEvents.Length -ne 0) {
+            $notificationState = $moduleState.NotifyEvents[0]
+            if ($notificationState.progressLog.Count -gt 0 -and $notificationState.progressLog -ne $null) {
+                #Write-Output $notificationState
+                for ($i = $startIndex; $i -lt $notificationState.progressLog.Count; $i++) {
+                    Write-Output $notificationState.progressLog[$i].Message
+                }
+                $startIndex = $notificationState.progressLog.Count - 1
+            }
         }
+        $cycleCount = $cycleCount + 1
+        Start-Sleep -s 3
+    }
+    while ($notificationState.finished -eq $null -and $cycleCount -lt 60) # stop processing after 3 min or when notifications had stopped $moduleState.NotifyEvents.Length -ne 0 -and
+
+    if($null -ne $needRestart -and $needRestart -gt 0){
+        Write-Output "Restarting website"
+        $moduleState = Invoke-RestMethod "$restartUrl" -Method Post -ContentType "application/json" -Headers $headers
     }
 }
-while (([string]::IsNullOrEmpty($notify.finished)) -and $cycleCount -lt 180)
-Write-Output "Sample data installation complete"
+catch {
+    $cycleCount = $cycleCount + 1
+    $message = $_.Exception.Message
+    Write-Output "Error: $message"
+    throw $_.Exception
+}
