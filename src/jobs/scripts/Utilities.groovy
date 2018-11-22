47ec7b9d5c686b0ac5ce37e9297f8c26b96fd1a8
Alexander Morogov <asmorogov@gmail.com>
2018-11-22 15:37:10 +0200

    hns FIX

--- src/jobs/scripts/Utilities.groovy
+++ src/jobs/scripts/Utilities.groovy
@@ -4,8 +4,8 @@ class Utilities {
 
     private static String DefaultSharedLibName = 'vc-pipeline'
     private static String DefaultAdminDockerPrefix = 'http://localhost'
-    private static Integer DefaultPlatformPort = 8090
-    private static Integer DefaultStorefrontPort = 8080
+    private static Integer DefaultPlatformPort = 8091
+    private static Integer DefaultStorefrontPort = 8081
     private static Integer DefaultSqlPort = 4567
 
     /**
