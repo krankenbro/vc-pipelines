b9e6392397a71446da06df338c04e373c69da660
Alexander Morogov <asmorogov@gmail.com>
2018-12-05 17:14:08 +0200

    install theme

--- resources/azure/vc-install-theme.ps1
+++ resources/azure/vc-install-theme.ps1
@@ -7,6 +7,6 @@ Param(
 
 Write-Output $platformContainer
 Write-Output $themeZip
-docker cp $themeZip ${platformContainer}:/vc-platform/
+docker cp "${themeZip}" ${platformContainer}:/vc-platform/
 docker exec $platformContainer powershell -Command "Remove-Item C:\vc-platform\App_Data\cms-content\Themes\Electronics\default\ -Force -Recurse"
 docker exec $platformContainer powershell -Command "Expand-Archive -Path C:\vc-platform\theme.zip -DestinationPath C:\vc-platform\App_Data\cms-content\Themes\Electronics\"
