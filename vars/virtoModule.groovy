c3ee0ec7b44e80ac932f56a14e01f5b7c8b22e8a
Alexander Morogov <asmorogov@gmail.com>
2018-12-07 12:04:23 +0200

    job

--- vars/virtoModule.groovy
+++ vars/virtoModule.groovy
@@ -166,8 +166,8 @@ def call(body) {
                 if (env.BRANCH_NAME == 'master') {
                     stage('Build platform and storefront') {
                         timestamps{
-                            build("../vc-platform/${env.BRANCH_NAME}", parameters: [string(name: 'isCaused', value: true)])
-                            build("../vc-storefront-core/${env.BRANCH_NAME}", parameters: [string(name: 'isCaused', value: true)])
+                            build(job: "../vc-platform/${env.BRANCH_NAME}", parameters: [string(name: 'isCaused', value: true)])
+                            build(job: "../vc-storefront-core/${env.BRANCH_NAME}", parameters: [string(name: 'isCaused', value: true)])
                         }
                     }
                 }
