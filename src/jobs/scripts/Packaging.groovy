623d17700b8bef5db748d00d37491668855da688
Alexander Morogov <asmorogov@gmail.com>
2018-12-10 11:10:50 +0200

    theme key fix

--- src/jobs/scripts/Packaging.groovy
+++ src/jobs/scripts/Packaging.groovy
@@ -249,7 +249,7 @@ class Packaging {
         def sqScannerMsBuildHome = context.tool 'Scanner for MSBuild'
         def fullJobName = Utilities.getRepoName(context)
         //\sonar-scanner-3.2.0.1227\\bin\\sonar-scanner.bat
-        context.bat "\"${sqScannerMsBuildHome}\\sonar-scanner-3.2.0.1227\\bin\\sonar-scanner.bat\" scan -Dsonar.projectKey=theme_default_${context.env.BRANCH_NAME}_${context.env.BUILD_NUMBER} -Dsonar.sources=. -Dsonar.branch=${context.env.BRANCH_NAME} -Dsonar.projectName=\"${fullJobName}\" -Dsonar.host.url=%SONAR_HOST_URL% -Dsonar.login=%SONAR_AUTH_TOKEN% -Dsonar.eslint.reportPaths=\"${context.env.WORKSPACE}@tmp\\report.json\""
+        context.bat "\"${sqScannerMsBuildHome}\\sonar-scanner-3.2.0.1227\\bin\\sonar-scanner.bat\" scan -Dsonar.projectKey=theme_default_${context.env.BRANCH_NAME} -Dsonar.sources=. -Dsonar.branch=${context.env.BRANCH_NAME} -Dsonar.projectName=\"${fullJobName}\" -Dsonar.host.url=%SONAR_HOST_URL% -Dsonar.login=%SONAR_AUTH_TOKEN% -Dsonar.eslint.reportPaths=\"${context.env.WORKSPACE}@tmp\\report.json\""
         //context.bat "\"${sqScannerMsBuildHome}\\sonar-scanner-3.2.0.1227\\bin\\sonar-scanner.bat\" end -Dsonar.login=%SONAR_AUTH_TOKEN%"
     }
 
